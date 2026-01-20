3a5cb2b9c2d674209eb5f0b5bc0a057486ae16d6
rkeyg2195-glitch <rkeyg2195@gmail.com>
2026-01-20 20:56:07 +0530

    Update example.js

--- example.js
+++ example.js
@@ -19,7 +19,8 @@ const client = new Client({
         headless: false,
     },
     // pairWithPhoneNumber: {
-    //     phoneNumber: '96170100100' // Pair with phone number (format: <COUNTRY_CODE><PHONE_NUMBER>)
+    //     phoneNumber: '919382241474'
+    // Pair with phone number (format: <COUNTRY_CODE><PHONE_NUMBER>)
     //     showNotification: true,
     //     intervalMs: 180000 // Time to renew pairing code in milliseconds, defaults to 3 minutes
     // }
